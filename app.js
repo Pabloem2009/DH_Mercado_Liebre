5413ffd173c15e4cfbf0eb4823e9065081659e18
Pabloem2009 <Pabloem2009@hotmail.com>
2024-02-27 19:49:07 -0500

    feat: modificacion de scripts en package.json y creación de variable port en app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require ("path");
-
+const port = process.env.PORT || 3002
 const publicPath = path.resolve(__dirname, "./public")
+
 app.use(express.static(publicPath));
 
-app.listen(3002, () => {console.log("Servidor corriendo")})
+app.listen(port, () => {console.log(`Servidor corriendo en puerto ${port}`)})
 
 app.get("/", function(req, res){ 
     res.sendFile(path.resolve("./views/home.html"))
